c1ec34860f95adc03b2d0b7defa27f64aea3b427
Raven <revanfauzi81@gmail.com>
2023-06-09 23:29:29 +0700

    Add circle animation

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let i = 0.2;
+    let circles = document.querySelectorAll('.rotate');
+    console.log(circles)
+    circles.forEach(circle => {
+        let animationDuration = Math.random() * (18 - 12) + 12;
+        console.log(animationDuration);
+        circle.style.animationDuration = animationDuration*i + 's';
+        i += 0.2;
+    });
+})
+
 function darkMode() {
     let selector = document.querySelector("body");
     let icon = document.querySelector(".mode");
