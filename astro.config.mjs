53357aa396ca4c92606775bca40545556529fe76
Revanfz <revanfauzi81@gmail.com>
2025-05-06 20:30:56 +0700

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://revanfz.github.io',
+  base: '/revanfz.github.io/',
   integrations: [vue()],
   outDir: './docs',
   vite: {
